61bb78965295811fa61c397bd576ad57c7c5b194
abderhman <abdohfny21000@gmail.com>
2023-01-03 08:49:21 +0200

    add

--- arch/script.js
+++ arch/script.js
@@ -2,12 +2,16 @@ let btn = document.querySelector(".btn");
 let btnn = document.querySelector("button");
 let a = document.querySelector(".btn div");
 let saleh = document.querySelector("nav ul");
+let printt = document.querySelector(".printt");
 btn.addEventListener("click", function () {
-    window.scrollTo({ top: 0, behavior: "smooth" });
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+printt.addEventListener("click", function () {
+  print();
 });
 
 btn.innerHTML = '<div style="color: white;">Up</div>';
 saleh.innerHTML +=
-'<li><a href="صالح.html">صالح</a></li>' ||
-'<li><a href="arch/صالح.html">صالح</a></li>';
+  '<li><a href="صالح.html">صالح</a></li>' ||
+  '<li><a href="arch/صالح.html">صالح</a></li>';
 
